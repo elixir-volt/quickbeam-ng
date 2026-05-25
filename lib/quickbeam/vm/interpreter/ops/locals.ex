ce08185ed5b5f39c5e51b022bc8b0f247e5a8c5a
Danila Poyarkov <dev@dannote.net>
2026-05-25 09:27:37 +0300

    Snapshot loop-scoped captured locals

--- lib/quickbeam/vm/interpreter/ops/locals.ex
+++ lib/quickbeam/vm/interpreter/ops/locals.ex
@@ -190,20 +190,15 @@ defmodule QuickBEAM.VM.Interpreter.Ops.Locals do
           vref_idx ->
             vrefs = elem(frame, Frame.var_refs())
 
-            case elem(vrefs, vref_idx) do
-              {:cell, _} ->
-                run(pc + 1, frame, stack, gas, ctx)
+            old_cell = elem(vrefs, vref_idx)
+            val = Closures.read_cell(old_cell)
+            new_ref = make_ref()
+            Heap.put_cell(new_ref, val)
 
-              old_cell ->
-                val = Closures.read_cell(old_cell)
-                new_ref = make_ref()
-                Heap.put_cell(new_ref, val)
+            frame =
+              put_elem(frame, Frame.var_refs(), put_elem(vrefs, vref_idx, {:cell, new_ref}))
 
-                frame =
-                  put_elem(frame, Frame.var_refs(), put_elem(vrefs, vref_idx, {:cell, new_ref}))
-
-                run(pc + 1, frame, stack, gas, ctx)
-            end
+            run(pc + 1, frame, stack, gas, ctx)
         end
       end
     end
